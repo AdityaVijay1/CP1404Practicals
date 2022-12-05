af71309b4dfd8eaa00ec4d92e11b7829a30d5beb
Aditya <84958533+AdityaVijay1@users.noreply.github.com>
2022-12-05 18:00:30 +0800

    Update state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,6 +4,8 @@ State names in a dictionary
 File needs reformatting
 """
 lengths = []
+# Creating function blocks will help the reusuability of the code
+
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
